5702a09fbf5def7ad01c4217111de6974c17200e
PronCan <twojini98@gmail.com>
2023-03-21 17:50:17 +0900

    backup

---
+++ _backup/src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/test',
+    name: 'testView',
+    props: true,
+    component: () => import(/* webpackChunkName: "about" */ '../views/TestView.vue')
+
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
