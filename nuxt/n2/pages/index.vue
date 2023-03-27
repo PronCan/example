0d56d006420e641f4e5235ac825b80ac9cd6768d
PronCan <twojini98@gmail.com>
2023-03-27 15:46:04 +0900

    create: nuxtjs example

---
+++ nuxt/n2/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <header>
+      <nuxt-link to="/">Home</nuxt-link>
+      <NuxtLink to="/about">About</NuxtLink>
+    </header>
+    <div class="image"> 
+      <img src="@/assets/img/img-04.jpg" />
+    </div>
+  
+    <TestContent/>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "IndexPage",
+
+}
+</script>
+
+<style scoped>
+  .image{border:5px solid red;}
+</style>
